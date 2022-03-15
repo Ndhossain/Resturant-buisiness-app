1ddb863616c5864e7aeca2723d224efcd53ad90f
Ndhossain <hhhssnahiid@gmail.com>
2022-03-16 04:06:21 +0600

    navbar work done

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,36 +1,57 @@
-import classes from "../styles/Menu.module.css"
-import buttonclasses from "../styles/SignupButton.module.css"
+import { useEffect, useState } from "react";
+import classes from "../styles/Menu.module.css";
+import buttonclasses from "../styles/SignupButton.module.css";
 import Button from "./Button";
 
 export default function Menu() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const handleMenuClick = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
   return (
     <>
-      <div className={classes.hamBurger}>
-        <h1 className="material-icons-outlined" style={{fontSize: "40px"}}>
-          menu
+      <div className={classes.hamBurger} onClick={handleMenuClick}>
+        <h1 className="material-icons-outlined" style={{ fontSize: "40px" }}>
+          {toggleMenu ? "close" : "menu"}
         </h1>
       </div>
-      <ul className={classes.menu}>
-        <li>
-          <a href="/Home.html">HOME</a>
-        </li>
-        <li>
-          <a href="/About.html">ABOUT</a>
-        </li>
-        <li>
-          <a href="/About.html">MENU</a>
-        </li>
-        <li>
-          <a href="/About.html">VOUCHER</a>
-        </li>
-        <li>
-          <a href="/About.html">CONTACTS</a>
-        </li>
-        <li>
-          <Button classes={buttonclasses} type="LOG IN" />
-          <Button classes={buttonclasses} type="SIGN UP" />
-        </li>
-      </ul>
+      {(toggleMenu || screenWidth > 1010) && (
+        <ul className={classes.menu}>
+          <li>
+            <a href="/Home.html">HOME</a>
+          </li>
+          <li>
+            <a href="/About.html">ABOUT</a>
+          </li>
+          <li>
+            <a href="/About.html">MENU</a>
+          </li>
+          <li>
+            <a href="/About.html">VOUCHER</a>
+          </li>
+          <li>
+            <a href="/About.html">CONTACTS</a>
+          </li>
+          <li>
+            <Button classes={buttonclasses} type="LOG IN" />
+            <Button classes={buttonclasses} type="SIGN UP" />
+          </li>
+        </ul>
+      )}
     </>
   );
 }
