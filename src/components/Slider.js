f5d86450e5741a93bcba81d9f0ab8bed3b68f345
Ndhossain <hhhssnahiid@gmail.com>
2022-03-21 15:41:18 +0600

    slider work done

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,13 +6,14 @@ import useFetch from "./hooks/useFetch";
 export default function Slider({ url }) {
   const [x, setX] = useState(0);
   const { loading, error, result } = useFetch(url, "GET");
+  console.log(x)
 
   function leftClick() {
-    x <= 0 ? setX(x - 16.65) : setX(0)
+    setX((prevX) => prevX - 16.65);
   }
 
   function rightClick() {
-    x >= -133.2 ? setX(x + 16.65) : setX(0)
+    setX((prevX) => prevX + 16.65);
   }
 
   const category = result ? result?.categories : [];
@@ -22,32 +23,28 @@ export default function Slider({ url }) {
       {loading && <div>Loading....</div>}
       {error && <div>An error occured!</div>}
       {!loading && !error && category.length > 0 && (
-        <div  onClick={leftClick} className={classes.slider}>
-          <div className={classes.Arrow}>
-            <h4 className={`material-icons-outlined`}>
-              navigate_before
-            </h4>
+        <div className={classes.slider}>
+          <div onClick={leftClick} className={classes.Arrow}>
+            <h4 className={`material-icons-outlined`}>navigate_before</h4>
           </div>
 
           <div className={classes.hider}>
-          <div
-            className={classes.cateItems}
-            style={{ transform: `translateX(${x}%)` }}
-          >
-            {category.map((item) => (
-              <CateItemSlider
-                key={item.idCategory}
-                title={item.strCategory}
-                imageSource={item.strCategoryThumb}
-              />
-            ))}
-          </div>
+            <div
+              className={classes.cateItems}
+              style={{ transform: `translateX(${x}%)` }}
+            >
+              {category.map((item) => (
+                <CateItemSlider
+                  key={item.idCategory}
+                  title={item.strCategory}
+                  imageSource={item.strCategoryThumb}
+                />
+              ))}
+            </div>
           </div>
 
           <div onClick={rightClick} className={classes.Arrow}>
-            <h4 className={`material-icons-outlined`}>
-              navigate_next
-            </h4>
+            <h4 className={`material-icons-outlined`}>navigate_next</h4>
           </div>
         </div>
       )}
