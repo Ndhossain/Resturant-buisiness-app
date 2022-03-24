2ae62e6432afe7ad7359e27338f36c50d24a6854
Ndhossain <hhhssnahiid@gmail.com>
2022-03-24 21:12:15 +0600

    mealitem showcase component work going on

--- src/components/Mealitems.js
+++ src/components/Mealitems.js
@@ -1,6 +1,7 @@
 import classes from "../styles/Mealitem.module.css"
 
-export default function Mealitems() {
+export default function Mealitems({mealValue}) {
+    console.log(mealValue);
     return (
         <div className={classes.itemsSection}>
             <h1>Hello World</h1>
