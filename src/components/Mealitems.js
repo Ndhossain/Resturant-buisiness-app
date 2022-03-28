21bd3b5e5ba68883cd06827ff85cf1fc3d4b4a94
Ndhossain <hhhssnahiid@gmail.com>
2022-03-28 15:46:08 +0600

    work is going on

--- src/components/Mealitems.js
+++ src/components/Mealitems.js
@@ -1,52 +1,39 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import classes from "../styles/Mealitem.module.css";
 
+export default function Mealitems({ mealValue, category }) {
+  const [response, setResponse] = useState({ myArray: [] });
 
-
-export default function Mealitems({mealValue, category}) {
-    const [response, setResponse] = useState(null);
+  useEffect(() => {
+      const myArray = [];
+    const url = category.map(
+      (item) =>
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${item.strCategory}`
+    );
+    const response = url.map((urls) =>
+     axios.get(urls)
+     .then((res)=>
+     console.log(res.data.meals))
+    );
     
-    const myArray = [];
-    const getItems = async (cate) => {
-        const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${cate}`
-        const response = await axios.get(url)
-        return response.data
-    }
+  }, [category]);
 
-    console.log(myArray);
-    
-    category.forEach(categories => {
-        getItems(categories.strCategory).then((res) => {
-            let newArray = myArray.concat(...res.meals)
-            // setResponse(newArray);
-        })
-    });
-    console.log(response);
+  console.log(response);
 
-    // useEffect(() => {
-    //     setResponse(myArray)
-    // },[myArray]
-    // )
-
-            
-
-    return (
-        <div className={classes.itemsSection}>
-            <div>
-
-                {/* {myarray.length < 0 && myarray.map((items) => (
-                    items.map((item) => (
-                        <div className={classes.products} key={item.idMeal}>
-                            <img src={item.strMealThumb} alt={item.strMeal} />
-                            <span>Title: {item.strMeal}</span>
-                            <span>calories</span>
-                            <span>Details</span>
-                        </div>
-                    ))
-                ))} */}
-            
+  return (
+    <div className={classes.itemsSection}>
+      <div>
+        {/* {response.length >= 0 &&
+          response.map((item) => (
+            <div className={classes.products} key={item.idMeal}>
+              <img src={item.strMealThumb} alt={item.strMeal} />
+              <span>Title: {item.strMeal}</span>
+              <span>calories</span>
+              <span>Details</span>
             </div>
-        </div>
-    )
+          ))} */}
+      </div>
+    </div>
+  );
 }
