7501cc29118319ea2706ba583a62d5e3b16741fd
Ndhossain <hhhssnahiid@gmail.com>
2022-03-29 02:46:09 +0600

    i am success to show the food items

--- src/components/Mealitems.js
+++ src/components/Mealitems.js
@@ -3,36 +3,42 @@ import { useEffect, useState } from "react";
 import classes from "../styles/Mealitem.module.css";
 
 export default function Mealitems({ mealValue, category }) {
-  const [response, setResponse] = useState({ myArray: [] });
+  const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(true)
+
+
 
   useEffect(() => {
-      const myArray = [];
     const url = category.map(
       (item) =>
         `https://www.themealdb.com/api/json/v1/1/filter.php?c=${item.strCategory}`
     );
-    const response = url.map((urls) =>
-     axios.get(urls)
-     .then((res)=>
-     console.log(res.data.meals))
-    );
-    
-  }, [category]);
 
-  console.log(response);
+    axios.all(url.map((url) => axios.get(url))).then(
+      (data) => setResponse(data),
+    );
+
+    if(loading) return setLoading(!loading);
+    
+  }, [category, loading]);
+
+  const result = response?.map((item) => item.data.meals)
 
   return (
     <div className={classes.itemsSection}>
       <div>
-        {/* {response.length >= 0 &&
-          response.map((item) => (
-            <div className={classes.products} key={item.idMeal}>
+        {loading && <div>Loading...</div>}
+        {!loading && result?.length >= 0 &&
+          result.map((items) => (
+            items.map((item) => (
+              <div className={classes.products} key={item.idMeal}>
               <img src={item.strMealThumb} alt={item.strMeal} />
               <span>Title: {item.strMeal}</span>
               <span>calories</span>
               <span>Details</span>
             </div>
-          ))} */}
+            ))
+          ))}
       </div>
     </div>
   );
