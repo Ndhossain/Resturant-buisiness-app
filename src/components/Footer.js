7e4a2f21f54da7b27b094086e9525270b54a9e60
Ndhossain <hhhssnahiid@gmail.com>
2022-04-02 16:10:42 +0600

    footer work is going on

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,28 @@
+import { FaFacebook, FaInstagram, FaYoutube, FaTwitter, } from "react-icons/fa";
 import classes from "../styles/Footer.module.css";
 import Logo from "./Logo";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faFacebook, faTwitter} from "@fortawesome/free-brand-svg-icons"
+
+
+console.log(FaFacebook);
 
 export default function Footer() {
-    return (
-        <footer className={classes.footer}>
-            <div className={classes.firstFooterPart}>
-                <Logo />
-                <p>We offers high-quality foods and the best delivery service, and the food market you can blindly trust</p>
-                <div className={classes.socialIcon}>
-                <FontAwesomeIcon icon={faFacebook} />
-                </div>
-            </div>
-            <div></div>
-            <div></div>
-        </footer>
-    )
+  return (
+    <footer className={classes.footer}>
+      <div className={classes.firstFooterPart}>
+        <Logo />
+        <p>
+          We offers high-quality foods and the best delivery service, and the
+          food market you can blindly trust
+        </p>
+        <div className={classes.socialIcon}>
+          <FaFacebook className={classes.icon1} />
+          <FaTwitter className={classes.icon2} />
+          <FaInstagram className={classes.icon3} />
+          <FaYoutube className={classes.icon4} />
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </footer>
+  );
 }
-
