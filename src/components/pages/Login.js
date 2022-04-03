92942934376a5df1057b564a30be176652dbc9a5
Ndhossain <hhhssnahiid@gmail.com>
2022-04-03 16:27:04 +0600

    working on login function

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,20 +1,39 @@
 import classes from "../../styles/Login.module.css";
 
-export default function Login() {
+export default function Login({ activity, events, signupEvent }) {
   return (
-    <div className={classes.login}>
-      <span className="material-icons-outlined">close</span>
+    <div
+      className={classes.login}
+      style={{ display: `${activity ? "block" : "none"}` }}
+    >
+      <h1
+        onClick={events}
+        style={{ cursor: `pointer`, padding: `.5em`, float: `right` }}
+        className="material-icons-outlined"
+      >
+        close
+      </h1>
       <div className={classes.inputSection}>
-         <div className={classes.inputItems}>
-            <h1>Login</h1>
-            <br/>
-            <br/>
-        <form>
-          <input type="text" placeholder="ENTER EMAIL" />
-          <input type="password" placeholder="PASSWORD" />
-          <input className={classes.submitButton} type="submit" value="Submit" />
-        </form>
-        <p>Don't have an account? Signup.</p>
+        <div className={classes.inputItems}>
+          <h1>Login</h1>
+          <br />
+          <br />
+          <form>
+            <input type="text" placeholder="ENTER EMAIL" />
+            <input type="password" placeholder="PASSWORD" />
+            <input
+              className={classes.submitButton}
+              type="submit"
+              value="Submit"
+            />
+          </form>
+          <p>
+            Don't have an account?{" "}
+            <span style={{ cursor: `pointer` }} onClick={signupEvent}>
+              Signup
+            </span>
+            .
+          </p>
         </div>
       </div>
     </div>
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,28 +1,48 @@
-import classes from "../../styles/Signup.module.css"
+import classes from "../../styles/Signup.module.css";
 
-export default function Signup() {
-    return (
-    <div className={classes.signup}>
-    <span className="material-icons-outlined">close</span>
-    <div className={classes.inputSection}>
-       <div className={classes.inputItems}>
+export default function Signup({ activity, events, loginEvent }) {
+  return (
+    <div
+      style={{ display: `${activity ? "block" : "none"}` }}
+      className={classes.signup}
+    >
+      <h1
+        onClick={events}
+        style={{ cursor: `pointer`, padding: `.5em`, float: `right` }}
+        className="material-icons-outlined"
+      >
+        close
+      </h1>
+      <div className={classes.inputSection}>
+        <div className={classes.inputItems}>
           <h1>Sign up Now</h1>
-          <br/>
-          <br/>
-      <form>
-      <input type="text" placeholder="FIRST NAME" />
-      <input type="text" placeholder="LAST NAME" />
-        <input type="text" placeholder="ENTER EMAIL" />
-        <input type="password" placeholder="PASSWORD" />
-        <input type="text" placeholder="CONFIRM PASSWORD" />
-        <div style={{marginBottom: `1em`}}>
-        <input type="checkbox"/>  <span>I agree to the Terms &amp; Conditions</span>
+          <br />
+          <br />
+          <form>
+            <input type="text" placeholder="FIRST NAME" />
+            <input type="text" placeholder="LAST NAME" />
+            <input type="text" placeholder="ENTER EMAIL" />
+            <input type="password" placeholder="PASSWORD" />
+            <input type="text" placeholder="CONFIRM PASSWORD" />
+            <div style={{ marginBottom: `1em` }}>
+              <input type="checkbox" />{" "}
+              <span>I agree to the Terms &amp; Conditions</span>
+            </div>
+            <input
+              className={classes.submitButton}
+              type="submit"
+              value="Submit"
+            />
+          </form>
+          <p>
+            Already have an account?{" "}
+            <span style={{ cursor: `pointer` }} onClick={loginEvent}>
+              Login
+            </span>{" "}
+            instead.
+          </p>
         </div>
-        <input className={classes.submitButton} type="submit" value="Submit" />
-      </form>
-      <p>Already have an account? Login instead.</p>
       </div>
     </div>
-  </div>
-    )
+  );
 }
