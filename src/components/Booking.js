8020af3ae70d44969b0d3ef4e2c9fb6ac3ea984e
Ndhossain <hhhssnahiid@gmail.com>
2022-04-10 16:17:06 +0600

    booking page work going on

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -6,6 +6,7 @@ export default function Booking({reservationType}) {
             <div className={classes.reservationDetails}>
                 <h3>Set Your {reservationType} Now</h3>
                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
+                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
             </div>
         </div>
     )
