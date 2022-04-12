77507cc7c1907d28338bb74cdbe981634cdc3039
Ndhossain <hhhssnahiid@gmail.com>
2022-04-12 19:31:51 +0600

    working on office

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,13 +1,48 @@
-import classes from "../styles/Booking.module.css"
+import { useState } from "react";
+import classes from "../styles/Booking.module.css";
 
-export default function Booking({reservationType}) {
-    return (
-        <div className={classes.booking}>
-            <div className={classes.reservationDetails}>
-                <h3>Set Your {reservationType} Now</h3>
-                <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters.</p>
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-            </div>
+export default function Booking({ reservationType }) {
+  const [more, setMore] = useState(true);
+
+  const handleClick = () => {
+    setMore(!more);
+  };
+
+  return (
+    <div className={classes.booking}>
+      <div className={classes.reservationDetails}>
+        <h3>Set Your {reservationType} Now</h3>
+        <br />
+        <span>
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters.
+        </span>
+        {more ? (
+          <span
+            onClick={handleClick}
+            style={{ color: `#b17d04`, cursor: `pointer` }}
+          >
+            More+
+          </span>
+        ) : (
+          <span style={{ marginTop: `24px` }}>
+            <br />
+            <br />
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.
+          </span>
+        )}
+        <div className={classes.iconWith}>
+          <span className="material-icons-outlined" style={{fontSize: `1em`, marginRight: `.5em`}}>
+            restaurant
+          </span>
+          <span>Hello World</span>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
