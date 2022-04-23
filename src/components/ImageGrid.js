81390299d923bf7db8548b9ad413b3f9dd1cc36f
Ndhossain <hhhssnahiid@gmail.com>
2022-04-23 15:28:34 +0600

    gallery page work going on

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,11 +1,27 @@
+import { useState } from "react";
+import FullImage from "./FullImage";
+
 export default function ImageGrid({ images = [], classes }) {
+  const [indexNo, setIndexNo] = useState();
+  const [fullScreen, setFullScreen] = useState(false);
+
   return (
-    <div className={classes.imageGrid}>
-    {images?.map((image, index) => (
-      <div key={index} className={classes.images}>
-        <img src={image.url} alt="Gallery Images" />
+    <>
+      <div className={classes.imageGrid}>
+        {images?.map((image, index) => (
+          <div key={index} className={classes.images}>
+            <img
+              onClick={() => {
+                setIndexNo(index);
+                setFullScreen(true);
+              }}
+              src={image.url}
+              alt="Gallery Images"
+            />
+          </div>
+        ))}
       </div>
-    ))}
-  </div>
+      {fullScreen && <FullImage images={images} indexNo={indexNo} />}
+    </>
   );
 }
