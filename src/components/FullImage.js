0b45c591fd839299c17f7fa6a38a7a850dee7b36
Ndhossain <hhhssnahiid@gmail.com>
2022-04-24 15:08:39 +0600

    review page work

--- src/components/FullImage.js
+++ src/components/FullImage.js
@@ -1,11 +1,38 @@
 import classes from "../styles/FullImage.module.css";
 
-export default function FullImage({ images, indexNo }) {
+export default function FullImage({
+  images,
+  indexNo,
+  setFullScreen,
+  setIndexNo,
+}) {
+  console.log(indexNo);
+  console.log(images);
   return (
     <div className={classes.FullImage}>
-      <span className="material-icons-outlined">arrow_back_ios</span>
+      <h1
+        onClick={() => setFullScreen(false)}
+        className={`${classes.close} material-icons-outlined`}
+      >
+        close
+      </h1>
+      <span
+        onClick={() => setIndexNo((prev) => (prev > 0 ? prev - 1 : 0))}
+        style={{ cursor: `pointer`, padding: `.5em` }}
+        className="material-icons-outlined"
+      >
+        arrow_back_ios
+      </span>
       <img src={images[indexNo].url} alt={`full`} />
-      <span className="material-icons-outlined">arrow_forward_ios</span>
+      <span
+        onClick={() =>
+          setIndexNo((prev) => (prev < images.length - 1 ? prev + 1 : prev))
+        }
+        style={{ cursor: `pointer`, padding: `.5em` }}
+        className="material-icons-outlined"
+      >
+        arrow_forward_ios
+      </span>
     </div>
   );
 }
