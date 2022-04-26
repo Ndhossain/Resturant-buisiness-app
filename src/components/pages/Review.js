dad9110bda8cfd70c47d0f90228fb87f9e81ddc8
Ndhossain <hhhssnahiid@gmail.com>
2022-04-26 15:29:35 +0600

    review page work going on

--- src/components/pages/Review.js
+++ src/components/pages/Review.js
@@ -1,25 +1,45 @@
-import { useState } from "react";
+import {
+  get,
+  getDatabase,
+  orderByKey,
+  query,
+  ref,
+  set,
+} from "firebase/database";
+import { useEffect, useState } from "react";
 import MessageForm from "../MessageForm";
-import { getDatabase, ref, set } from "firebase/database";
 
 export default function Review() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [reviewNo, setReviewNo] = useState()
+
+  useEffect(() => {
+    async function review() {
+      const db = getDatabase();
+      const galleryRef = ref(db, `information/review/`);
+      const galleryQuery = query(galleryRef, orderByKey());
+      const snapShot = await get(galleryQuery);
+      setReviewNo(snapShot.val().length)
+    }
+  
+    review()
+  }, [])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const db = getDatabase();
-    const messageRef = ref(
-      db,
-      `information/review/${1}`
-    );
+    console.log(db);
+    const messageRef = ref(db, `information/review/${reviewNo ? reviewNo : 0}`);
+
+    console.log(messageRef);
 
     await set(messageRef, {
-      email: email,
+      profession: email,
       name: name,
-      message: message,
+      review: message,
     });
 
     setName("");
